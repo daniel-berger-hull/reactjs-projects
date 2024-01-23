2fc2804b47045b3cfdd3e0529f3aed813c979543
Daniel Berger <daniel.berger.hull@gmail.com>
2024-01-22 20:53:52 -0500

    Adding a Test of buttons

---
+++ basicreactjs/src/components/buttonsForm/ButtonsForm.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './ButtonsForm.css';
+
+class ButtonsForm extends React.Component {
+
+    render()  {
+
+        return ( <div className="buttons-form">
+                    <h1>Daniel</h1>
+                </div>
+        );
+        
+    }
+
+}
+
+export default ButtonsForm;
+
